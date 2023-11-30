019fb7e55673c07f27d96976148f635ba2001ad5
Votre Nom <tchjohanna@icloud.com>
2023-11-30 20:56:17 +0100

    Refactorisé et optimisé le composant Form pour inclure la gestion des données de formulaire, amélioré la lisibilité avec des commentaires explicatifs, et ajusté les propTypes et defaultProps pour une meilleure fiabilité.

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,34 +1,54 @@
+// Importations des hooks et composants nécessaires
 import { useCallback, useState } from "react";
 import PropTypes from "prop-types";
 import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+// Fonction simulant un appel API pour l'envoi de contact
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
 
+/**
+ * Form - Un composant pour gérer la soumission d'un formulaire de contact.
+ * @param {Function} onSuccess - Callback appelé en cas de succès.
+ * @param {Function} onError - Callback appelé en cas d'erreur.
+ */
 const Form = ({ onSuccess, onError }) => {
+  // État pour gérer si le formulaire est en train d'être envoyé
   const [sending, setSending] = useState(false);
+
+  // Gestion de la soumission du formulaire
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
-      setSending(true);
-      // We try to call mockContactApi
+      const formData = new FormData(evt.target);
+      console.log(Object.fromEntries(formData)); // Afficher les données du formulaire dans la console
+
+      setSending(true); // Activer l'état d'envoi
+
+      // Essayer d'envoyer le formulaire via l'API mockée
       try {
         await mockContactApi();
-        setSending(false);
+        setSending(false); // Désactiver l'état d'envoi après succès
+        onSuccess(); // Appel du callback onSuccess
       } catch (err) {
-        setSending(false);
-        onError(err);
+        setSending(false); // Désactiver l'état d'envoi en cas d'erreur
+        onError(err); // Appel du callback onError avec l'erreur
       }
     },
     [onSuccess, onError]
   );
+
+  // Rendu du composant de formulaire
   return (
     <form onSubmit={sendContact}>
       <div className="row">
         <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
+          <Field placeholder="" label="Nom" name="Nom" />
+          <Field placeholder="" label="Prénom" name="Prénom" />
           <Select
             selection={["Personel", "Entreprise"]}
             onChange={() => null}
@@ -36,8 +56,17 @@ const Form = ({ onSuccess, onError }) => {
             type="large"
             titleEmpty
           />
-          <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+          <Field
+            placeholder=""
+            label="Email"
+            name="Email"
+            type={FIELD_TYPES.InputEmail}
+          />
+          <Button
+            type={BUTTON_TYPES.SUBMIT}
+            disabled={sending}
+            onClick={onSuccess}
+          >
             {sending ? "En cours" : "Envoyer"}
           </Button>
         </div>
@@ -46,6 +75,7 @@ const Form = ({ onSuccess, onError }) => {
             placeholder="message"
             label="Message"
             type={FIELD_TYPES.TEXTAREA}
+            name="msg"
           />
         </div>
       </div>
@@ -53,14 +83,16 @@ const Form = ({ onSuccess, onError }) => {
   );
 };
 
+// Définition des propTypes
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
+// Définition des defaultProps
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
