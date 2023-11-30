2d255ec115f3ca0e82c6917bc4187209555be40a
Votre Nom <tchjohanna@icloud.com>
2023-11-30 20:13:21 +0100

    ajouter  le filtrage par type et le tri par date dans Events

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,39 +13,35 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
-  const changeType = (evtType) => {
+
+  const handlePageChange = (page) => setCurrentPage(page);
+  const handleChangeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  const typeList = new Set(data?.events.map((event) => event.type));
+
+  const filteredEvents = data?.events
+    .filter(event => !type || event.type === type)
+    .slice((currentPage - 1) * PER_PAGE, currentPage * PER_PAGE)
+    .sort((a, b) => new Date(a.date) - new Date(b.date)) || [];
+
+  const totalPages = Math.ceil((data?.events.length || 0) / PER_PAGE);
+  const typeList = new Set(data?.events.map(event => event.type));
+
   return (
     <>
-      {error && <div>An error occured</div>}
-      {data === null ? (
-        "loading"
+      {error && <div>An error occurred</div>}
+      {!data ? (
+        <div>Loading...</div>
       ) : (
         <>
           <h3 className="SelectTitle">Catégories</h3>
           <Select
             selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
+            onChange={handleChangeType}
           />
           <div id="events" className="ListContainer">
-            {filteredEvents.map((event) => (
+            {filteredEvents.map(event => (
               <Modal key={event.id} Content={<ModalEvent event={event} />}>
                 {({ setIsOpened }) => (
                   <EventCard
@@ -60,9 +56,12 @@ const EventList = () => {
             ))}
           </div>
           <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+            {[...Array(totalPages)].map((_, n) => (
+              <a 
+                key={n} 
+                href="#events" 
+                onClick={() => handlePageChange(n + 1)}
+              >
                 {n + 1}
               </a>
             ))}
--- src/containers/ModalEvent/index.js
+++ src/containers/ModalEvent/index.js
@@ -1,40 +1,63 @@
 import PropTypes from "prop-types";
-
 import "./style.scss";
 
+/**
+ * ModalEvent - Component to display detailed information about an event.
+ * It shows the event's cover image, title, period, description,
+ * number of participants, and a list of services (prestations) offered.
+ *
+ * @param {Object} props - Component props.
+ * @param {Object} props.event - Event data to display.
+ */
 const ModalEvent = ({ event }) => (
-    <div className="ModalEvent">
-      <div className="ModalEvent__imageContainer">
-        <img
-          data-testid="card-image-testid"
-          src={event.cover}
-          alt={event.title}
-        />
-      </div>
-      <div className="ModalEvent__title">
-        <div className="ModalEvent__titleLabel">{event.title}</div>
-        <div className="ModalEvent__titlePeriode">{event.periode}</div>
-      </div>
-      <div className="ModalEvent__descriptionContainer">
-        <h3>Description</h3>
-        <div>{event.description}</div>
-      </div>
-      <div className="ModalEvent__descriptionContainer">
-        <h3>Participants</h3>
-        <div>{event.nb_guesses} participants</div>
-      </div>
-      <div className="ModalEvent__descriptionContainer">
-        <h3>Prestations</h3>
-        {event.prestations.map((presta) => (
-          <div key={presta}>{presta}</div>
-        ))}
-      </div>
+  <div className="ModalEvent">
+    {/* Image Container */}
+    <div className="ModalEvent__imageContainer">
+      <img
+        data-testid="card-image-testid"
+        src={event.cover}
+        alt={event.title}
+      />
     </div>
-  );
 
+    {/* Title and Period */}
+    <div className="ModalEvent__title">
+      <div className="ModalEvent__titleLabel">{event.title}</div>
+      <div className="ModalEvent__titlePeriode">{event.periode}</div>
+    </div>
+
+    {/* Description */}
+    <div className="ModalEvent__descriptionContainer">
+      <h3>Description</h3>
+      <div>{event.description}</div>
+    </div>
+
+    {/* Number of Participants */}
+    <div className="ModalEvent__descriptionContainer">
+      <h3>Participants</h3>
+      <div>{event.nb_guesses} participants</div>
+    </div>
+
+    {/* List of Services (Prestations) */}
+    <div className="ModalEvent__descriptionContainer">
+      <h3>Prestations</h3>
+      {event.prestations.map((presta) => (
+        <div key={presta}>{presta}</div>
+      ))}
+    </div>
+  </div>
+);
+
+// Defining propTypes for the ModalEvent component.
 ModalEvent.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  event: PropTypes.any.isRequired,
-}
+  event: PropTypes.shape({
+    cover: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    periode: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    nb_guesses: PropTypes.number.isRequired,
+    prestations: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
+};
 
 export default ModalEvent;
