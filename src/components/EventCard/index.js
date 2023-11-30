cbedcf395d0d931bc8e28ac8337fc40181872e27
Votre Nom <tchjohanna@icloud.com>
2023-11-30 20:47:21 +0100

    Propriétés par défaut dans la déclaration de fonction plus props ne sont plus considérés comme essentiels pour le fonctionnement du composant dans EventCard

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -3,43 +3,56 @@ import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
+/**
+ * EventCard - Un composant pour afficher une carte d'événement.
+ * 
+ * @param {string} imageSrc - Source de l'image de la carte.
+ * @param {string} imageAlt - Texte alternatif pour l'image.
+ * @param {Date} date - La date de l'événement.
+ * @param {string} title - Le titre de l'événement.
+ * @param {string} label - Étiquette supplémentaire pour la carte (comme le type d'événement).
+ * @param {boolean} small - Si vrai, la carte est affichée dans un style plus petit.
+ * @param {object} props - Autres props à passer à la carte.
+ */
 const EventCard = ({
   imageSrc,
-  imageAlt,
-  date = new Date(),
+  imageAlt = "image", // Valeur par défaut pour imageAlt
+  date = new Date(),   // Valeur par défaut pour date
   title,
   label,
-  small = false,
+  small = false,       // Valeur par défaut pour small
   ...props
 }) => (
-    <div
-      data-testid="card-testid"
-      className={`EventCard${small ? " EventCard--small" : ""}`}
-      {...props}
-    >
-      <div className="EventCard__imageContainer">
-        <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
-        <div className="EventCard__label">{label}</div>
-      </div>
-      <div className="EventCard__descriptionContainer">
-        <div className="EventCard__title">{title}</div>
-        <div className="EventCard__month">{getMonth(date)}</div>
-      </div>
+  <div
+    data-testid="card-testid"
+    className={`EventCard${small ? " EventCard--small" : ""}`}
+    {...props}
+  >
+    <div className="EventCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+      <div className="EventCard__label">{label}</div>
     </div>
-  );
+    <div className="EventCard__descriptionContainer">
+      <div className="EventCard__title">{title}</div>
+      <div className="EventCard__month">{getMonth(date)}</div>
+    </div>
+  </div>
+);
 
+// Définition des types de props et des props obligatoires
 EventCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
   imageAlt: PropTypes.string,
   date: PropTypes.instanceOf(Date).isRequired,
-  title: PropTypes.string.isRequired,
+  title: PropTypes.string,
   small: PropTypes.bool,
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
 
+// Définition des valeurs par défaut pour les props non obligatoires
 EventCard.defaultProps = {
   imageAlt: "image",
   small: false,
-}
+};
 
 export default EventCard;
